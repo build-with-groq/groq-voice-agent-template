1f388669d9f54ce8eb3a6dd2a575add0b8bdf38d
benank <bankiel@groq.com>
2025-09-17 14:29:53 -0700

    Add wrangler to deploy to cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import path from "path"
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    svelte()
+    svelte(),
+    cloudflare()
   ],
   base: './',
   resolve: {
